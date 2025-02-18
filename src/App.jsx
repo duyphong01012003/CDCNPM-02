6d9f9885f4ff5e138898e7b2770eae227986f48d
VietAnh219 <phungvietanh21092003@gmail.com>
2025-02-18 11:04:04 +0700

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Login from './pages/Login'
+
+function App() {
+  return <Login />
+}
+
+export default App
+// Quan li nhan vien
+// Quan li tai khoan
+// Trang chu
+// login
+// Quan li cong viec
+// Quan li tai lieu 
